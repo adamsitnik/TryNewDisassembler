607b3872ac6c8b64d2324e8630dd983f50e4cc7c
Adam Sitnik <adam.sitnik@gmail.com>
2020-01-09 15:48:07 +0100

    sample project that is configured to use the new improved disassembler

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace TryNewDisassembler
+{
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, CreateConfig());
+
+        static IConfig CreateConfig()
+            => DefaultConfig.Instance
+                .AddJob(
+                    Job.Dry // Dry job runs the benchmark just once
+                       .WithEnvironmentVariable("COMPlus_TieredCompilation", "0") // When using Tiered JIT the ClrMD reports addresses of Tier 0 method, not Tier 1 https://github.com/dotnet/BenchmarkDotNet/issues/899
+                       .AsDefault())
+                .AddDiagnoser(
+                    new DisassemblyDiagnoser(
+                        new DisassemblyDiagnoserConfig(
+                            maxDepth: 2, // you can change it to a bigger value if you want to get more framework methods disassembled
+                            exportGithubMarkdown: true)));
+    }
+
+    public class WithDifferentCalls
+    {
+        [Benchmark]
+        [Arguments(int.MaxValue)]
+        public void Benchmark(int someArgument)
+        {
+            if (someArgument != int.MaxValue)
+                throw new InvalidOperationException("Wrong value of the argument!!");
+
+            Static();
+            Instance();
+            Recursive();
+            Virtual();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)] public static void Static() { }
+
+        [MethodImpl(MethodImplOptions.NoInlining)] public void Instance() { }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Recursive()
+        {
+            if (new Random(123).Next(0, 10) == 11) // never true
+                Recursive();
+        }
+
+        public virtual void Virtual() { }
+    }
+}
